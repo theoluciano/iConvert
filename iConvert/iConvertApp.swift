cbf38b03bb6a4e4001be2777417db3b7705d2df6
Theo Luciano <62947477+theoluciano@users.noreply.github.com>
2024-10-11 17:47:49 -0500

    Initial Commit

---
+++ iConvert/iConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConvertApp.swift
+//  iConvert
+//
+//  Created by Theo Luciano on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct iConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iConvertTests/iConvertTests.swift
@@ -0,0 +1,17 @@
+//
+//  iConvertTests.swift
+//  iConvertTests
+//
+//  Created by Theo Luciano on 10/11/24.
+//
+
+import Testing
+@testable import iConvert
+
+struct iConvertTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
